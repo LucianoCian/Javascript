e32f5a674a1835acdc2af76babf60328d3d52a74
Luciano Cian <117651620+LucianoCian@users.noreply.github.com>
2023-04-17 20:16:42 -0300

    Add files via upload

---
+++ sintaxis.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Luciano",
+    23,
+    true,
+    new Date(1999, 05, 22),
+    { 
+        titulo: "El monje que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996, 0, 1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+  },
+];
